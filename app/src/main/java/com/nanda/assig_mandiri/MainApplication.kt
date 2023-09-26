163f5da39d84780eda71a9f75b2c862f2b7eb81d
nandasamudera <samudera.nanda@gmail.com>
2023-09-27 00:22:50 +0700

    - setup naviagation

--- app/src/main/java/com/nanda/assig_mandiri/MainApplication.kt
+++ app/src/main/java/com/nanda/assig_mandiri/MainApplication.kt
@@ -11,6 +11,7 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        setupKoin()
     }
 
     private fun setupKoin() {
--- app/src/main/java/com/nanda/assig_mandiri/feature/MainActivity.kt
+++ app/src/main/java/com/nanda/assig_mandiri/feature/MainActivity.kt
@@ -2,11 +2,28 @@ package com.nanda.assig_mandiri.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
 import com.nanda.assig_mandiri.R
+import com.nanda.assig_mandiri.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var graph: NavGraph
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        graph = inflater.inflate(R.navigation.nav_graph_main)
+        graph.setStartDestination(R.id.fragment_news_category)
+        navHostFragment.navController.graph = graph
     }
 }
