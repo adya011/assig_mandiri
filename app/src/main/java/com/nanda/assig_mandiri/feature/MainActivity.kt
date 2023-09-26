d9b0e83349e5765549f235771558251dcb5139e0
nandasamudera <samudera.nanda@gmail.com>
2023-09-26 23:32:38 +0700

    - features

--- app/src/main/java/com/nanda/assig_mandiri/feature/MainActivity.kt
+++ app/src/main/java/com/nanda/assig_mandiri/feature/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nanda.assig_mandiri
+package com.nanda.assig_mandiri.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nanda.assig_mandiri.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/nanda/assig_mandiri/feature/article/NewsArticleFragment.kt
@@ -0,0 +1,25 @@
+package com.nanda.assig_mandiri.feature.article
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nanda.assig_mandiri.R
+
+class NewsArticleFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_article, container, false)
+    }
+
+
+}
