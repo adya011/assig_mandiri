90e9ecb97acd744afb5460ba967d9cf05103dbcf
nandasamudera <samudera.nanda@gmail.com>
2023-09-29 13:48:08 +0700

    - page size not hardcoded

--- data/remote/src/main/java/com/nanda/remote/api/NewsApi.kt
+++ data/remote/src/main/java/com/nanda/remote/api/NewsApi.kt
@@ -3,6 +3,7 @@ package com.nanda.remote.api
 import com.nanda.remote.model.EverythingDto
 import com.nanda.remote.model.TopHeadlineDto
 import com.nanda.remote.util.API_KEY
+import com.nanda.remote.util.PAGE_SIZE
 import com.nanda.remote.util.PUBLISHED_AT
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
@@ -15,7 +16,7 @@ interface NewsApi {
         @Query("sources") source: String,
         @Query("q") query: String = "",
         @Query("page") page: Int = 1,
-        @Query("pageSize") pageSize: Int = 20,
+        @Query("pageSize") pageSize: Int = PAGE_SIZE,
         @Query("sortBy") sortBy: String = PUBLISHED_AT,
         @Query("apiKey") apiKey: String = API_KEY
     ): Deferred<Response<EverythingDto>>
--- data/remote/src/main/java/com/nanda/remote/util/Constants.kt
+++ data/remote/src/main/java/com/nanda/remote/util/Constants.kt
@@ -2,7 +2,7 @@ package com.nanda.remote.util
 
 const val BASE_URL = "http://newsapi.org/v2/"
 const val API_KEY = "ab3b43a30a7e43d29731c81d2d8ca6a4" //secondary: 21dd529edc3242e9a227f9e06e8db478
-
+const val PAGE_SIZE = 10
 const val PUBLISHED_AT = "publishedAt"
 const val TECHCRUNCH = "techcrunch"
 
